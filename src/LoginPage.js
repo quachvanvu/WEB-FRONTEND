df9bb86e8a70f1dace827623af76d46b16c182ea
quachvanvu <vuq41783@gmail.com>
2023-11-06 18:49:20 +0700

    first commit

---
+++ src/LoginPage.js
@@ -0,0 +1,153 @@
+import React, { useState, useEffect } from "react";
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import Paper from "@mui/material/Paper";
+import Typography from "@mui/material/Typography";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+
+const LoginPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [emailError, setEmailError] = useState(false);
+  const [formError, setFormError] = useState("");
+  const [isRegistering, setIsRegistering] = useState(false);
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+    const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    setEmailError(!emailPattern.test(e.target.value));
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleConfirmPasswordChange = (e) => {
+    setConfirmPassword(e.target.value);
+  };
+
+  const handleRegisterClick = () => {
+    setIsRegistering(true);
+  };
+
+  const handleLoginFormSubmit = (e) => {
+    e.preventDefault();
+    if (emailError) {
+      setFormError("Email không hợp lệ");
+    } else if (email && password) {
+      setFormError("");
+    } else {
+      setFormError("Vui lòng nhập đầy đủ thông tin");
+    }
+  };
+
+  const handleRegisterFormSubmit = (e) => {
+    e.preventDefault();
+    if (emailError || !email || !password || password !== confirmPassword) {
+      setFormError("Vui lòng kiểm tra thông tin đăng ký");
+    } else {
+      // Xử lý đăng ký tại đây
+      setFormError("");
+    }
+  };
+
+  return (
+    <Container
+      maxWidth="md"
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh"
+      }}
+    >
+      <Paper
+        elevation={3}
+        style={{
+          padding: "20px",
+          height: "50%",
+          maxWidth: "700px",
+          margin: "0 auto"
+        }}
+      >
+        <Grid container>
+          <Grid
+            item
+            xs={6}
+            style={{
+              backgroundImage: `url(${process.env.PUBLIC_URL}/magicLogo.png)`,
+              backgroundSize: "cover"
+            }}
+          ></Grid>
+          <Grid item xs={6} style={{paddingTop : '30px'}}>
+            <div style={{ padding: "16px" }}>
+              <form
+                onSubmit={
+                  isRegistering
+                    ? handleRegisterFormSubmit
+                    : handleLoginFormSubmit
+                }
+              >
+                <TextField
+                  style={{ marginBottom: "10px" }}
+                  label="Email"
+                  fullWidth
+                  value={email}
+                  onChange={handleEmailChange}
+                  error={emailError}
+                  helperText={emailError ? "Email không hợp lệ" : ""}
+                />
+                <TextField
+                  style={{ marginBottom: "10px" }}
+                  label="Mật khẩu"
+                  type="password"
+                  fullWidth
+                  value={password}
+                  onChange={handlePasswordChange}
+                />
+                {isRegistering && (
+                  <TextField
+                    style={{ marginBottom: "10px" }}
+                    label="Xác nhận mật khẩu"
+                    type="password"
+                    fullWidth
+                    value={confirmPassword}
+                    onChange={handleConfirmPasswordChange}
+                  />
+                )}
+                <Button
+                  variant="contained"
+                  color="primary"
+                  fullWidth
+                  type="submit"
+                >
+                  {isRegistering ? "Đăng ký" : "Đăng nhập"}
+                </Button>
+                <p style={{ textAlign: "center" }}></p>
+              </form>
+              {formError && (
+                <Typography variant="body2" color="error" align="center">
+                  {formError}
+                </Typography>
+              )}
+              {!isRegistering && (
+                <Button
+                  variant="outlined"
+                  color="primary"
+                  fullWidth
+                  onClick={handleRegisterClick}
+                >
+                  Đăng ký
+                </Button>
+              )}
+            </div>
+          </Grid>
+        </Grid>
+      </Paper>
+    </Container>
+  );
+};
+
+export default LoginPage;
