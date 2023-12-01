004cc7a1f094c421a4e6fc72ba3bd0344d94abf2
phamtu14 <20020493@vnu.edu.vn>
2023-12-01 20:54:26 +0700

    add role protect

--- src/pages/StaffGathering.js
+++ src/pages/StaffGathering.js
@@ -12,6 +12,15 @@ function StaffGathering() {
   const [xacnhanAnchorEl, setXacnhanAnchorEl] = React.useState(null);
   const [taodonAnchorEl, setTaodonAnchorEl] = React.useState(null);
 
+  const accessToken = window.localStorage.getItem('accessToken');
+
+  const headers = {
+    'Content-Type': 'application/json',
+    'AccessToken': accessToken,
+  };
+
+  const userRole = window.localStorage.getItem('userRole');
+
   const handleMenuOpen = (event) => {
     setAnchorEl(event.currentTarget);
   };
@@ -33,108 +42,114 @@ function StaffGathering() {
     setTaodonAnchorEl(null);
   };
   const handleLogout = () => {
+    window.localStorage.removeItem('accessToken');
+    window.localStorage.removeItem('refreshToken');
     window.location.href = '/';
   };
-  return (
-    <div>
-      <AppBar position="static" 
-      style={{ backgroundColor: '#2196f3', boxShadow: '0px 2px 5px rgba(0, 0, 0, 0.1)' }}
-      >
-        <Toolbar>
-          <IconButton color="inherit">
-            <Home />
-          </IconButton>
-          <Typography variant="h6">Trang chủ</Typography>
-          <div style={{ flexGrow: 1 }} />
-          <IconButton color="inherit" onClick={handleMenuOpen}>
-            <AccountCircle />
-          </IconButton>
-          <Menu
-            anchorEl={anchorEl}
-            keepMounted
-            open={Boolean(anchorEl)}
-            onClose={handleMenuClose}
-          >
-            <MenuItem onClick={handleMenuClose}>
-              <ListItemIcon>
-                <Storage />
-              </ListItemIcon>
-              <ListItemText primary="Lãnh đạo công ty" />
-            </MenuItem>
-          </Menu>
-        </Toolbar>
-      </AppBar>
-
-      <Grid container style={{ height : '91vh'}}>
-        <Grid item xs={3} style={{ 
-            backgroundColor: '#1e88e5', 
-            boxShadow: '0px 2px 5px rgba(0, 0, 0, 0.1)', padding: '10px' }}
+  if (userRole === 'staffGathering') {
+    return (
+      <div>
+        <AppBar position="static" 
+        style={{ backgroundColor: '#2196f3', boxShadow: '0px 2px 5px rgba(0, 0, 0, 0.1)' }}
+        >
+          <Toolbar>
+            <IconButton color="inherit">
+              <Home />
+            </IconButton>
+            <Typography variant="h6">Trang chủ</Typography>
+            <div style={{ flexGrow: 1 }} />
+            <IconButton color="inherit" onClick={handleMenuOpen}>
+              <AccountCircle />
+            </IconButton>
+            <Menu
+              anchorEl={anchorEl}
+              keepMounted
+              open={Boolean(anchorEl)}
+              onClose={handleMenuClose}
             >
-        <nav>
-          <ul>
-            <li>
-              <Button startIcon={<CheckCircleIcon />} onClick={handleXacnhanMenuOpen} 
+              <MenuItem onClick={handleMenuClose}>
+                <ListItemIcon>
+                  <Storage />
+                </ListItemIcon>
+                <ListItemText primary="Lãnh đạo công ty" />
+              </MenuItem>
+            </Menu>
+          </Toolbar>
+        </AppBar>
+  
+        <Grid container style={{ height : '91vh'}}>
+          <Grid item xs={3} style={{ 
+              backgroundColor: '#1e88e5', 
+              boxShadow: '0px 2px 5px rgba(0, 0, 0, 0.1)', padding: '10px' }}
+              >
+          <nav>
+            <ul>
+              <li>
+                <Button startIcon={<CheckCircleIcon />} onClick={handleXacnhanMenuOpen} 
+                  style={{ color : '#fff', paddingTop : '30px'}}
+                  >
+                  Xác nhận
+                </Button>
+                <Menu
+                  anchorEl={xacnhanAnchorEl}
+                  open={Boolean(xacnhanAnchorEl)}
+                  onClose={handleSubMenuClose}
+                >
+                  <MenuItem>
+                    <ListItemIcon>
+                      <Storage />
+                    </ListItemIcon>
+                    <ListItemText primary="Đơn hàng đi từ điểm giao dịch chuyển đến" />
+                  </MenuItem>
+                  <MenuItem>
+                    <ListItemIcon>
+                      <Gavel />
+                    </ListItemIcon>
+                    <ListItemText primary="Đơn hàng nhận về từ điểm tập kết khác" />
+                  </MenuItem>
+                </Menu>
+              </li>
+              <li>
+                <Button startIcon={<AddCircleOutlineIcon />} onClick={handleTaodonMenuOpen}
                 style={{ color : '#fff', paddingTop : '30px'}}
                 >
-                Xác nhận
-              </Button>
-              <Menu
-                anchorEl={xacnhanAnchorEl}
-                open={Boolean(xacnhanAnchorEl)}
-                onClose={handleSubMenuClose}
+                  Tạo đơn
+                </Button>
+                <Menu
+                  anchorEl={taodonAnchorEl}
+                  open={Boolean(taodonAnchorEl)}
+                  onClose={handleSubMenuClose}
+                >
+                  <MenuItem>
+                    <ListItemIcon>
+                      <Storage />
+                    </ListItemIcon>
+                    <ListItemText primary="Chuyển hàng đến điểm tập kết đích" />
+                  </MenuItem>
+                  <MenuItem>
+                    <ListItemIcon>
+                      <Gavel />
+                    </ListItemIcon>
+                    <ListItemText primary="Chuyển hàng đến điểm giao dịch đích" />
+                  </MenuItem>
+                </Menu>
+              </li>
+              <li>
+              <Button startIcon={<ExitToApp />} onClick={handleLogout} 
+              style={{ color : '#fff', paddingTop : '30px', marginTop : '380px'}}
               >
-                <MenuItem>
-                  <ListItemIcon>
-                    <Storage />
-                  </ListItemIcon>
-                  <ListItemText primary="Đơn hàng đi từ điểm giao dịch chuyển đến" />
-                </MenuItem>
-                <MenuItem>
-                  <ListItemIcon>
-                    <Gavel />
-                  </ListItemIcon>
-                  <ListItemText primary="Đơn hàng nhận về từ điểm tập kết khác" />
-                </MenuItem>
-              </Menu>
-            </li>
-            <li>
-              <Button startIcon={<AddCircleOutlineIcon />} onClick={handleTaodonMenuOpen}
-              style={{ color : '#fff', paddingTop : '30px'}}
-              >
-                Tạo đơn
-              </Button>
-              <Menu
-                anchorEl={taodonAnchorEl}
-                open={Boolean(taodonAnchorEl)}
-                onClose={handleSubMenuClose}
-              >
-                <MenuItem>
-                  <ListItemIcon>
-                    <Storage />
-                  </ListItemIcon>
-                  <ListItemText primary="Chuyển hàng đến điểm tập kết đích" />
-                </MenuItem>
-                <MenuItem>
-                  <ListItemIcon>
-                    <Gavel />
-                  </ListItemIcon>
-                  <ListItemText primary="Chuyển hàng đến điểm giao dịch đích" />
-                </MenuItem>
-              </Menu>
-            </li>
-            <li>
-            <Button startIcon={<ExitToApp />} onClick={handleLogout} 
-            style={{ color : '#fff', paddingTop : '30px', marginTop : '380px'}}
-            >
-            Đăng xuất
-          </Button>
-            </li>
-          </ul>
-        </nav>
-        </Grid>
-        </Grid>
-    </div>
-  );
+              Đăng xuất
+            </Button>
+              </li>
+            </ul>
+          </nav>
+          </Grid>
+          </Grid>
+      </div>
+    );
+  } else {
+    return <div>You are not allow to this action</div>
+  }
 }
 
 export default StaffGathering;
--- src/pages/StaffTransaction.js
+++ src/pages/StaffTransaction.js
@@ -12,6 +12,15 @@ function StaffTransaction() {
   const [taodonAnchorEl, setTaodonAnchorEl] = React.useState(null);
   const [xacnhanAnchorEl, setXacnhanAnchorEl] = React.useState(null);
 
+  const accessToken = window.localStorage.getItem('accessToken');
+
+  const headers = {
+    'Content-Type': 'application/json',
+    'AccessToken': accessToken,
+  };
+  
+  const userRole = window.localStorage.getItem('userRole');
+
   const handleMenuOpen = (event) => {
     setAnchorEl(event.currentTarget);
   };
@@ -38,133 +47,139 @@ function StaffTransaction() {
     setXacnhanAnchorEl(null);
   };
   const handleLogout = () => {
+    window.localStorage.removeItem('accessToken');
+    window.localStorage.removeItem('refreshToken');
     window.location.href = '/';
   };
-  return (
-    <div>
-      <AppBar position="static" 
-      style={{ backgroundColor: '#2196f3', boxShadow: '0px 2px 5px rgba(0, 0, 0, 0.1)' }}
-      >
-        <Toolbar>
-          <IconButton color="inherit">
-            <Home />
-          </IconButton>
-          <Typography variant="h6">Trang chủ</Typography>
-          <div style={{ flexGrow: 1 }} />
-          <IconButton color="inherit" onClick={handleMenuOpen}>
-            <AccountCircle />
-          </IconButton>
-          <Menu
-            anchorEl={anchorEl}
-            keepMounted
-            open={Boolean(anchorEl)}
-            onClose={handleMenuClose}
-          >
-            <MenuItem onClick={handleMenuClose}>
-              <ListItemIcon>
-                <Storage />
-              </ListItemIcon>
-              <ListItemText primary="Lãnh đạo công ty" />
-            </MenuItem>
-          </Menu>
-        </Toolbar>
-      </AppBar>
-
-      <Grid container style={{ height : '91vh'}}>
-        <Grid item xs={3} style={{ 
-            backgroundColor: '#1e88e5', 
-            boxShadow: '0px 2px 5px rgba(0, 0, 0, 0.1)', padding: '10px' }}
+  if (userRole === 'staffTransaction') {
+    return (
+      <div>
+        <AppBar position="static" 
+        style={{ backgroundColor: '#2196f3', boxShadow: '0px 2px 5px rgba(0, 0, 0, 0.1)' }}
+        >
+          <Toolbar>
+            <IconButton color="inherit">
+              <Home />
+            </IconButton>
+            <Typography variant="h6">Trang chủ</Typography>
+            <div style={{ flexGrow: 1 }} />
+            <IconButton color="inherit" onClick={handleMenuOpen}>
+              <AccountCircle />
+            </IconButton>
+            <Menu
+              anchorEl={anchorEl}
+              keepMounted
+              open={Boolean(anchorEl)}
+              onClose={handleMenuClose}
             >
-        <nav>
-          <ul>
-            <li>
-              <Button startIcon={<HowToRegOutlinedIcon />} onClick={handleGhinhanMenuOpen}
-               style={{ color : '#fff', paddingTop : '30px'}}
-               >
-                Ghi nhận
-              </Button>
-              <Menu
-                anchorEl={ginhanAnchorEl}
-                open={Boolean(ginhanAnchorEl)}
-                onClose={handleSubMenuClose}
+              <MenuItem onClick={handleMenuClose}>
+                <ListItemIcon>
+                  <Storage />
+                </ListItemIcon>
+                <ListItemText primary="Lãnh đạo công ty" />
+              </MenuItem>
+            </Menu>
+          </Toolbar>
+        </AppBar>
+  
+        <Grid container style={{ height : '91vh'}}>
+          <Grid item xs={3} style={{ 
+              backgroundColor: '#1e88e5', 
+              boxShadow: '0px 2px 5px rgba(0, 0, 0, 0.1)', padding: '10px' }}
               >
-                <MenuItem>
-                  <ListItemIcon>
-                    <Gavel />
-                  </ListItemIcon>
-                  <ListItemText primary="Điểm giao dịch" />
-                </MenuItem>
-                <MenuItem>
-                  <ListItemIcon>
-                    <Storage />
-                  </ListItemIcon>
-                  <ListItemText primary="Điểm tập kết" />
-                </MenuItem>
-              </Menu>
-            </li>
-            <li>
-              <Button startIcon={<AddCircleOutlineIcon />} onClick={handleTaodonMenuOpen} 
+          <nav>
+            <ul>
+              <li>
+                <Button startIcon={<HowToRegOutlinedIcon />} onClick={handleGhinhanMenuOpen}
+                 style={{ color : '#fff', paddingTop : '30px'}}
+                 >
+                  Ghi nhận
+                </Button>
+                <Menu
+                  anchorEl={ginhanAnchorEl}
+                  open={Boolean(ginhanAnchorEl)}
+                  onClose={handleSubMenuClose}
+                >
+                  <MenuItem>
+                    <ListItemIcon>
+                      <Gavel />
+                    </ListItemIcon>
+                    <ListItemText primary="Điểm giao dịch" />
+                  </MenuItem>
+                  <MenuItem>
+                    <ListItemIcon>
+                      <Storage />
+                    </ListItemIcon>
+                    <ListItemText primary="Điểm tập kết" />
+                  </MenuItem>
+                </Menu>
+              </li>
+              <li>
+                <Button startIcon={<AddCircleOutlineIcon />} onClick={handleTaodonMenuOpen} 
+                  style={{ color : '#fff', paddingTop : '30px'}}
+                  >
+                  Tạo đơn
+                </Button>
+                <Menu
+                  anchorEl={taodonAnchorEl}
+                  open={Boolean(taodonAnchorEl)}
+                  onClose={handleSubMenuClose}
+                >
+                  <MenuItem>
+                    <ListItemIcon>
+                      <Storage />
+                    </ListItemIcon>
+                    <ListItemText primary="Trưởng điểm tập kết" />
+                  </MenuItem>
+                  <MenuItem>
+                    <ListItemIcon>
+                      <Gavel />
+                    </ListItemIcon>
+                    <ListItemText primary="Trưởng điểm giao dịch" />
+                  </MenuItem>
+                </Menu>
+              </li>
+              <li>
+                <Button startIcon={<CheckCircleIcon />} onClick={handlXacnhanMenuOpen}
                 style={{ color : '#fff', paddingTop : '30px'}}
                 >
-                Tạo đơn
-              </Button>
-              <Menu
-                anchorEl={taodonAnchorEl}
-                open={Boolean(taodonAnchorEl)}
-                onClose={handleSubMenuClose}
+                  Xác nhận
+                </Button>
+                <Menu
+                  anchorEl={xacnhanAnchorEl}
+                  open={Boolean(xacnhanAnchorEl)}
+                  onClose={handleSubMenuClose}
+                >
+                  <MenuItem>
+                    <ListItemIcon>
+                      <Storage />
+                    </ListItemIcon>
+                    <ListItemText primary="Hàng gửi" />
+                  </MenuItem>
+                  <MenuItem>
+                    <ListItemIcon>
+                      <Gavel />
+                    </ListItemIcon>
+                    <ListItemText primary="Hàng nhận" />
+                  </MenuItem>
+                </Menu>
+              </li>
+              <li>
+              <Button startIcon={<ExitToApp />} onClick={handleLogout} 
+              style={{ color : '#fff', paddingTop : '30px', marginTop : '380px'}}
               >
-                <MenuItem>
-                  <ListItemIcon>
-                    <Storage />
-                  </ListItemIcon>
-                  <ListItemText primary="Trưởng điểm tập kết" />
-                </MenuItem>
-                <MenuItem>
-                  <ListItemIcon>
-                    <Gavel />
-                  </ListItemIcon>
-                  <ListItemText primary="Trưởng điểm giao dịch" />
-                </MenuItem>
-              </Menu>
-            </li>
-            <li>
-              <Button startIcon={<CheckCircleIcon />} onClick={handlXacnhanMenuOpen}
-              style={{ color : '#fff', paddingTop : '30px'}}
-              >
-                Xác nhận
-              </Button>
-              <Menu
-                anchorEl={xacnhanAnchorEl}
-                open={Boolean(xacnhanAnchorEl)}
-                onClose={handleSubMenuClose}
-              >
-                <MenuItem>
-                  <ListItemIcon>
-                    <Storage />
-                  </ListItemIcon>
-                  <ListItemText primary="Hàng gửi" />
-                </MenuItem>
-                <MenuItem>
-                  <ListItemIcon>
-                    <Gavel />
-                  </ListItemIcon>
-                  <ListItemText primary="Hàng nhận" />
-                </MenuItem>
-              </Menu>
-            </li>
-            <li>
-            <Button startIcon={<ExitToApp />} onClick={handleLogout} 
-            style={{ color : '#fff', paddingTop : '30px', marginTop : '380px'}}
-            >
-            Đăng xuất
-          </Button>
-            </li>
-          </ul>
-        </nav>
-        </Grid>
-        </Grid>
-    </div>
-  );
+              Đăng xuất
+            </Button>
+              </li>
+            </ul>
+          </nav>
+          </Grid>
+          </Grid>
+      </div>
+    );
+  } else {
+    return <div>You are not allow to this action</div>
+  }
 }
 
 export default StaffTransaction;
